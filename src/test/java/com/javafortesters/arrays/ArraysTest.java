9f2381fea7918387bfe0cf90ef6eeb0a85a32a1a
Rosalyn Goh <rosalyn.goh@gmail.com>
2017-01-11 21:50:34 -0800

    rest of Arrays Looping Tests

--- src/test/java/com/javafortesters/arrays/ArraysTest.java
+++ src/test/java/com/javafortesters/arrays/ArraysTest.java
@@ -30,4 +30,72 @@ public class ArraysTest {
 
         assertEquals("|Monday|Tuesday|Wednesday|Thursday|Friday", days);
     }
+
+    @Test
+    public void forLoopNoInitialize(){
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days ="";
+
+        int i=0;
+        for(; i<5; i++){
+            days = days + "|" + workdays[i];
+        }
+
+        assertEquals("|Monday|Tuesday|Wednesday|Thursday|Friday", days);
+    }
+
+    @Test
+    public void forLoopNoInitNoIncrement(){
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days ="";
+
+        int i=0;
+        for(; i<5; ){
+            days = days + "|" + workdays[i];
+            i++;
+        }
+
+        assertEquals("|Monday|Tuesday|Wednesday|Thursday|Friday", days);
+    }
+
+    @Test
+    public void forLoopNothingElse(){
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days ="";
+
+        int i=0;
+        for(; ; ){
+            days = days + "|" + workdays[i];
+            i++;
+            if(i>=5) break;
+        }
+
+        assertEquals("|Monday|Tuesday|Wednesday|Thursday|Friday", days);
+    }
+
+    @Test
+    public void forEachWithIndex(){
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days ="";
+
+        int dayindex =0;
+        for(String workday : workdays){
+            days = days + "|" + workday;
+            System.out.println("found " + workday + " at position " + dayindex);
+            dayindex++;
+        }
+    }
+
+    @Test
+    public void forLoopWithLength(){
+        String[] workdays = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+        String days ="";
+
+        for(int i=0; i<workdays.length ; i++){
+            days = days + "|" + workdays[i];
+        }
+
+        assertEquals(5, workdays.length);
+        assertEquals("|Monday|Tuesday|Wednesday|Thursday|Friday", days);
+    }
 }
